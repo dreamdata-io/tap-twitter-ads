660ae354167f31d0cf28189b9964b15a96696706
Adam <przywarty.it@gmail.com>
2020-03-11 16:40:49 +0100

    Remove temporary credentials file. Take tokens from config.json or ask for new ones and dispose them

--- tap_twitter_ads/__init__.py
+++ tap_twitter_ads/__init__.py
@@ -26,71 +26,54 @@ REQUIRED_CONFIG_KEYS = [
 CREDENTIALS_FILENAME = "twitter.json"
 
 
-def get_credentials():
-    """ Get credentials """
-
-    credential_path = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-        CREDENTIALS_FILENAME,
-    )
-    credentials = ""
-    if os.path.exists(credential_path):
-        with open(credential_path, "r") as file:
-            twitter_credentials = file.read()
-            if twitter_credentials:
-                credentials = json.loads(twitter_credentials)
-
-    if not credentials or credentials == "":
-        # Get new credentials
-        credentials_file = open(credential_path, "w+")
-
-        # Step 1: Obtain a request token which will identify you (the client) in the next step.
-        # At this stage you will only need your consumer key and secret.
-        oauth = OAuth1Session(
-            CONFIG["consumer_key"],
-            client_secret=CONFIG["consumer_secret"],
-            callback_uri="http://localhost:3000/oauth/validate",
-        )
-        fetch_response = oauth.fetch_request_token(CONFIG["request_token_url"])
-        resource_owner_key = fetch_response.get("oauth_token")
-        resource_owner_secret = fetch_response.get("oauth_token_secret")
-
-        # Step 2: Obtain authorization from the user (resource owner) to access their protected resources.
-        # This is commonly done by redirecting the user to a specific url to which you add the request token as a query parameter.
-        # Note that not all services will give you a verifier even if they should.
-        # Also the oauth_token given here will be the same as the one in the previous step.
-
-        authorization_url = oauth.authorization_url(CONFIG["authorize_url"])
-        print("Please go here and authorize,", authorization_url)
-        redirect_response = input(
-            "Paste the full redirect URL here (with token and verifier): "
-        )
-        oauth_response = oauth.parse_authorization_response(redirect_response)
-
-        oauth_verifier = oauth_response.get("oauth_verifier")
-
-        # Step 3: Obtain an access token from the OAuth provider. Save this token as it can be re-used later.
-        # In this step we will re-use most of the credentials obtained uptil this point.
-        oauth = OAuth1Session(
-            CONFIG["consumer_key"],
-            client_secret=CONFIG["consumer_secret"],
-            resource_owner_key=resource_owner_key,
-            resource_owner_secret=resource_owner_secret,
-            verifier=oauth_verifier,
-        )
-        oauth_tokens = oauth.fetch_access_token(CONFIG["access_token_url"])
-
-        new_credentials = {
-            "oauth_token": oauth_tokens.get("oauth_token"),
-            "oauth_token_secret": oauth_tokens.get("oauth_token_secret"),
+def get_credentials() -> dict:
+    if CONFIG["oauth_token"] and CONFIG["oauth_secret_token"]:
+        return {
+            "oauth_token": CONFIG["oauth_token"],
+            "oauth_secret_token": CONFIG["oauth_secret_token"],
         }
 
-        credentials_file.write(json.dumps(new_credentials))
-        credentials_file.close()
+    print("No oauth credentials in config.json. Getting new credentials.")
 
-        credentials = new_credentials
+    # Step 1: Obtain a request token which will identify you (the client) in the next step.
+    # At this stage you will only need your consumer key and secret.
+    oauth = OAuth1Session(
+        CONFIG["consumer_key"],
+        client_secret=CONFIG["consumer_secret"],
+        callback_uri="http://localhost:3000/oauth/validate",
+    )
+    fetch_response = oauth.fetch_request_token(CONFIG["request_token_url"])
+    resource_owner_key = fetch_response.get("oauth_token")
+    resource_owner_secret = fetch_response.get("oauth_token_secret")
 
-    return credentials
+    # Step 2: Obtain authorization from the user (resource owner) to access their protected resources.
+    # This is commonly done by redirecting the user to a specific url to which you add the request token as a query parameter.
+    # Note that not all services will give you a verifier even if they should.
+    # Also the oauth_token given here will be the same as the one in the previoushttp://localhost:3000/oauth/validate?oauth_token=5A_yPAAAAAABC-8IAAABcMpAXz4&oauth_verifier=ADO4HGkcRKfnKZUp3DOQppHwmxXeuEfL step.
+    authorization_url = oauth.authorization_url(CONFIG["authorize_url"])
+    print("Please go here and authorize,", authorization_url)
+    redirect_response = input(
+        "Paste the full redirect URL here (with token and verifier): "
+    )
+    oauth_response = oauth.parse_authorization_response(redirect_response)
+
+    oauth_verifier = oauth_response.get("oauth_verifier")
+
+    # Step 3: Obtain an access token from the OAuth provider. Save this token as it can be re-used later.
+    # In this step we will re-use most of the credentials obtained uptil this point.
+    oauth = OAuth1Session(
+        CONFIG["consumer_key"],
+        client_secret=CONFIG["consumer_secret"],
+        resource_owner_key=resource_owner_key,
+        resource_owner_secret=resource_owner_secret,
+        verifier=oauth_verifier,
+    )
+    oauth_tokens = oauth.fetch_access_token(CONFIG["access_token_url"])
+
+    return {
+        "oauth_token": oauth_tokens.get("oauth_token"),
+        "oauth_token_secret": oauth_tokens.get("oauth_token_secret"),
+    }
 
 
 def do_discover():
